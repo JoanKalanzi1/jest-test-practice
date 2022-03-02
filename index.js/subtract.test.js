a821c3294b72ad6513c06a05ed846487ad379996
JoanKalanzi <joan.kalanzi@snyk.io>
2022-03-02 16:28:08 +0000

    first commit

---
+++ index.js/subtract.test.js
@@ -0,0 +1,6 @@
+const { test, expect } = require('@jest/globals')
+const subtract = require('./subtract')
+
+test('subtracts 2-1 to equal 1', () => {
+    expect(subtract(2, 1)).toBe(1);
+})
---
+++ index.js/sum.test.js
@@ -0,0 +1,6 @@
+
+const sum = require('./sum')
+
+test('add two numbers properly', () => {
+expect(sum(1, 2)).toBe(3)
+})
